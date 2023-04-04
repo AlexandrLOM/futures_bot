dc283ae19e8611309d31cd64e47f20e051a0c239
Oleksandr Lukashchuk <lukashchuk.o.m@gmail.com>
2023-04-04 13:31:25 +0300

    init project

---
+++ src/main/java/com/lom/FuturesBotApplication.java
@@ -0,0 +1,13 @@
+package com.lom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuturesBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FuturesBotApplication.class, args);
+    }
+
+}
